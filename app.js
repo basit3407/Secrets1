6604de9ac0b73ee49c963de1a0b4524fb8bb3bd9
unknown <basit3407@gmail.com>
2020-11-19 14:51:58 +0500

    checking

---
+++ app.js
@@ -0,0 +1,106 @@
+//jshint esversion:6
+
+const express = require("express");
+const _ = require("lodash");
+const bp = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
+const ejs = require("ejs");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(express.static('public'));
+
+app.use(bp.urlencoded({
+  extended: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/SecretsDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log("we are connected to DataBase");
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String
+});
+
+const secret = "This is my secret";
+userSchema.plugin( encrypt, {secret: secret,  encryptedFeilds:  ['password'] ,excludeFromEncryption:  ["username"] } ) ;
+
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.route("/register").get((req, res) => {
+    res.render("register");
+  })
+  .post((req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    const newUser = new User({
+      username: username,
+      password: password
+
+    });
+    newUser.save(err => {
+      if (!err) {
+        res.render("secrets");
+      } else {
+        console.log(error);
+      }
+    });
+  });
+
+app.route("/login").get((req, res) => {
+    res.render("login");
+  })
+  .post((req, res) => {
+
+    const username = req.body.username;
+    const password = req.body.password;
+
+    User.findOne({
+      username: username
+    }, (err, result) => {
+      if (!err) {
+        if (result) {
+
+          if (result.password === password) {
+            res.render("secrets");
+          } else {
+            console.log("wrong password,please try again");
+          }
+
+        } else {
+          console.log("Invalid username,please try again or if you are new user please register first");
+        }
+      } else {
+        console.log(err);
+      }
+    });
+  });
+
+
+
+
+
+
+
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
